0633c26af4d716c527b7c771e102abac18cf6ba7
ksehat <kan.sehat@gmail.com>
2022-12-24 17:14:10 +0330

    json parser is added.

--- guru/API_guru2.py
+++ guru/API_guru2.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, json
+from flask import Flask, request
+import json
 from guru2 import get_booking_page
-import logging
+from json_parser import json_parser
 
 app = Flask(__name__)
 
@@ -11,6 +12,9 @@ def home():
         result = get_booking_page('THR', 'AWZ', 1, 0, 0, '1401-10-01', '1401-10-02')
         return result
     if (request.method == 'POST'):
+        data = json.loads(request.data.decode('utf-8'))
+        json_parser(data)
+
         print('')
 
 
