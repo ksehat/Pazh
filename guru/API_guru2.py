3ddbf6e0ff7d3eaf6855c6852257cd427857d6ef
ksehat <kan.sehat@gmail.com>
2022-12-25 13:20:16 +0330

    deleted unwanted options.

--- guru/API_guru2.py
+++ guru/API_guru2.py
@@ -1,21 +1,16 @@
 from flask import Flask, request
 import json
 from guru2 import get_booking_page
-from json_parser import json_parser
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def home():
-    if (request.method == 'GET'):
-        result = get_booking_page('THR', 'AWZ', 1, 0, 0, '1401-10-01', '1401-10-02')
-        return result
     if (request.method == 'POST'):
         data = json.loads(request.data.decode('utf-8'))
-        json_parser(data)
-
-        print('')
+        result = get_booking_page(data)
+        return result
 
 
 
