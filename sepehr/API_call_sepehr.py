9113227f66a3006c7d3324422c58bda67ffe063c
ksehat <kan.sehat@gmail.com>
2023-01-02 13:02:11 +0330

    nothing important.

--- sepehr/API_call_sepehr.py
+++ sepehr/API_call_sepehr.py
@@ -3,9 +3,9 @@ import json
 import pandas as pd
 
 data = {
-  "origin": "ADU",
-  "destination": "THR",
-  "days": 3
+  "origin": "THR",
+  "destination": "MHD",
+  "days": 4
 }
 
 r = requests.post(url='http://192.168.40.155:2000', data=json.dumps(data))
