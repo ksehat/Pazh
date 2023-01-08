e2bb6f97aa21e206258e1f952ff6adc33fd20c21
ksehat <kan.sehat@gmail.com>
2023-01-08 13:27:58 +0330

    The file transfered to the request_APIs.py.

--- sepehr/API_login_token.py
+++ sepehr/API_login_token.py
@@ -1,14 +1,6 @@
 import requests
 import json
 
-dict1 = {
-    "username": "100737",
-    "password": "4380",
-    "applicationType": 961,
-    "iP": "1365"
-}
-dict1_json = json.dumps(dict1)
-r = requests.post(url='http://192.168.20.243:8081/api/Authentication/RequestToken', json=dict1)
-token = json.loads(r.text)['token']
-expire_date = json.loads(r.text)['expires']
+
+
 
