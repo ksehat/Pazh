1c863e5e64d6c29e32088204bc89aef8f6b79fe2
ksehat <kan.sehat@gmail.com>
2023-01-08 13:44:33 +0330

    Comments are added.

--- sepehr/main.py
+++ sepehr/main.py
@@ -1,15 +1,14 @@
 import os
-import json
 import pandas as pd
 import ast
-from request_APIs import call_sepehr, call_login_token, call_input_setting_db
 import json
 from datetime import datetime as dt
 import datetime
+from request_APIs import call_sepehr, call_login_token, call_input_setting_db
 
 
 def main():
-    # start token and expiration date handling
+    # region token and expiration date handling
     if 'token_expire_date.txt' in os.listdir():
         with open('token_expire_date.txt', 'r') as f:
             te = f.read()
@@ -25,11 +24,13 @@ def main():
         expire_date = expire_date.split('T')[0]
         with open('token_expire_date.txt', 'w') as f:
             f.write(expire_date + 'token:' + token)
-    # end token and expiration date handling
+    # endregion
 
+    # region API request for input setting
     data = call_input_setting_db(token)
+    # endregion
 
-    # start controlling df_routes for handling start process time
+    # region controlling df_routes for handling start process time
     if 'routes_start_time.csv' in os.listdir():
         df_routes = pd.read_csv('routes_start_time.csv')
         df_routes['route'] = df_routes['route'].map(lambda x: ast.literal_eval(x))
@@ -40,17 +41,16 @@ def main():
             'start_process_time': [dt.now() - datetime.timedelta(1)] * len(
                 json.loads(data.text)['getAllRouteMonitoringResponseItemViewModels'])
         })
-    # end
+    # endregion
+
     for index, row in df_routes.iterrows():
         if ((dt.now() - row[1]).total_seconds() / 60) >= row[0]['interval']:
             result = call_sepehr(json.dumps(row[0]))
+            # TODO: API post method should be used here to post the results to the DB2
             if result:
                 df_routes.loc[index, 'start_process_time'] = dt.now()
-                df_routes.to_csv('routes_start_time.csv',index=False)
-            # TODO: API post method should be used here to post the results to the DB2
-            else:
-                # TODO: here we should add logging.
-                continue
+                df_routes.to_csv('routes_start_time.csv', index=False)
+            # TODO: here we should add logging for both error state and without error.
         else:
             continue
 
