b1dae09fffb987cd90923822fbb3a2d536eaedc3
ksehat <kan.sehat@gmail.com>
2023-01-09 09:51:35 +0330

    Logger is added to the main.py.

--- sepehr/main.py
+++ sepehr/main.py
@@ -2,13 +2,24 @@ import os
 import pandas as pd
 import ast
 import json
+import logging
 from datetime import datetime as dt
 import datetime
 from request_APIs import call_sepehr, call_login_token, call_input_setting_db
 
 
 def main():
+    # Create logger and assign handler
+    logging.basicConfig(filename='log.log', filemode='a', format="%(asctime)s|%(levelname)s|%(name)s|%(message)s")
+    logger = logging.getLogger("flight_scraper")
+    # handler = logging.FileHandler('log.log')
+    # handler.setFormatter(logging.Formatter())
+    # logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+    logger.info('Application started.')
     # region token and expiration date handling
+    logger.info('Token handling is started.')
     if 'token_expire_date.txt' in os.listdir():
         with open('token_expire_date.txt', 'r') as f:
             te = f.read()
@@ -24,13 +35,17 @@ def main():
         expire_date = expire_date.split('T')[0]
         with open('token_expire_date.txt', 'w') as f:
             f.write(expire_date + 'token:' + token)
+    logger.info('Token handling is OK.')
     # endregion
 
     # region API request for input setting
+    logger.info('Calling input setting API.')
     data = call_input_setting_db(token)
+    logger.info('Calling input setting API was successful.')
     # endregion
 
     # region controlling df_routes for handling start process time
+    logger.info('Handling routes_start_time.csv file.')
     if 'routes_start_time.csv' in os.listdir():
         df_routes = pd.read_csv('routes_start_time.csv')
         df_routes['route'] = df_routes['route'].map(lambda x: ast.literal_eval(x))
@@ -43,15 +58,23 @@ def main():
         })
     # endregion
 
+    logger.info('Looping over the routes started.')
     for index, row in df_routes.iterrows():
         if ((dt.now() - row[1]).total_seconds() / 60) >= row[0]['interval']:
+            logger.info(
+                f'Scraping the route {data["iataCodeOrigin"]} to {data["iataCodeDestination"]} for {data["monitoringDays"]} days(day) with interval of {data["interval"]} minutes started.')
             result = call_sepehr(json.dumps(row[0]))
+
             # TODO: API post method should be used here to post the results to the DB2
             if result:
+                logger.info(
+                    f'Scraping the route {data["iataCodeOrigin"]} to {data["iataCodeDestination"]} for {data["monitoringDays"]} days(day) with interval of {data["interval"]} minutes finished successfuly.')
                 df_routes.loc[index, 'start_process_time'] = dt.now()
                 df_routes.to_csv('routes_start_time.csv', index=False)
             # TODO: here we should add logging for both error state and without error.
         else:
+            logger.info(
+                f'Scraping the route {data["iataCodeOrigin"]} to {data["iataCodeDestination"]} for {data["monitoringDays"]} days(day) with interval of {data["interval"]} minutes finished successfuly.')
             continue
 
 
