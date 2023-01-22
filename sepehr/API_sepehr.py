5f99cd808bc7072997d724ad4cc2cd39a084c885
ksehat <kan.sehat@gmail.com>
2023-01-22 11:57:24 +0330

    port is changed.

--- sepehr/API_sepehr.py
+++ sepehr/API_sepehr.py
@@ -17,5 +17,5 @@ def home():
 # host_IP = f'{input("Please inset IP:")}'
 # host_port = f'{input("Please inset port:")}'
 if __name__ == '__main__':
-    app.run(host='192.168.40.155', port='2000')
+    app.run(host='192.168.40.155', port='3000')
 
