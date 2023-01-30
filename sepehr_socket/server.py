28d70f378becc530520603c3c8324b68d156105d
ksehat <kan.sehat@gmail.com>
2023-01-30 14:13:15 +0330

    the most important, MongoDB is added to the code to save all messages.

--- sepehr_socket/server.py
+++ sepehr_socket/server.py
@@ -1,15 +1,26 @@
+import requests
+import json
 from flask import Flask
 from flask_socketio import SocketIO, send, join_room, leave_room, emit
 from flask_cors import CORS
-import time
+import datetime as dt
 from waitress import serve
-
+from pymongo import MongoClient
+from cryptography.fernet import Fernet
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 # app.config['DEBUG'] = True
 socketio = SocketIO(app)
 CORS(app)
+
+# region DataBase handler
+client = MongoClient()
+mydatabase = client['history']
+mycollection = mydatabase['messages']
+
+
+# endregion
 
 
 @socketio.on('connect')
@@ -24,15 +35,35 @@ def on_disconnect():
 
 @socketio.on('message')
 def on_message(msg):
-    print(msg)
-    send({'msg':msg['msg'],'loc':msg['loc']['lat']+ ',' +msg['loc']['long']},room=msg['room'])
+    # print(msg)
+    send(msg, room=msg['room'])
+    try:
+        message = msg['msg']
+    except:
+        message = None
+    record = {
+        'user type': msg['usrType'],
+        'room': msg['room'],
+        'sender': msg['sender'],
+        'location': msg['loc'],
+        'message_text': message,
+        'message_time': dt.datetime.now(),
+    }
+    mydatabase.mycollection.insert_one(record)
 
 
 @socketio.on('join')
 def on_join(data):
     room = data['room']
-    join_room(room)
-    send('you join the room.', to=room)
+    token = data['token']
+    r = requests.get(
+        url='http://192.168.20.243:8083/api/Authentication/IsUserAuthorized',
+        headers={'Authorization': f'Bearer {token}'})
+    if json.loads(r.content)['success']:
+        join_room(room)
+        send('you join the room.', to=room)
+    else:
+        send({'error':True})
 
 
 @socketio.on('leave')
@@ -42,10 +73,7 @@ def on_leave(data):
     send('you leave the room.', to=room)
 
 
-def join_room_local(room):
-    join_room(room)
-
-
 if __name__ == "__main__":
     # socketio.run(app, allow_unsafe_werkzeug=True, host='192.168.40.155', port=8000)
-    serve(app, host='192.168.40.155', port=8001, threads=8)
+    serve(app, host='192.168.40.155', port=8001, url_scheme='https', threads=5000, connection_limit=20000,
+          channel_timeout=-1)
