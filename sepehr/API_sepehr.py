5f91507506f5ac9063f47f1d7f37eb70fdd6bf83
ksehat <kan.sehat@gmail.com>
2023-02-08 09:46:33 +0330

    response of API for errors are handled.

--- sepehr/API_sepehr.py
+++ sepehr/API_sepehr.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 import json
 from sepehr_scraper import get_booking_sepehr
 from waitress import serve
@@ -11,12 +11,16 @@ def home():
     if (request.method == 'POST'):
         data = json.loads(request.data)
         result = get_booking_sepehr(data)
-        return jsonify({'data': (result.to_json(orient='records', force_ascii=False))}).json
-
+        try:
+            return jsonify({'data': (result.to_json(orient='records', force_ascii=False))}).json
+        except:
+            return Response(
+                "Error in scraper",
+                status=400,
+            )
 
 
 # host_IP = f'{input("Please inset IP:")}'
 # host_port = f'{input("Please inset port:")}'
 if __name__ == '__main__':
     serve(app, host='192.168.40.155', port='3000')
-
